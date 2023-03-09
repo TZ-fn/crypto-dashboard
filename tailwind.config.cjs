7e2097a8a37207b88edd8b993044c9caf8991933
Tomek <tomekz@vp.pl>
2023-03-09 21:43:11 +0100

    feat: move the variables to the Tailwind config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'text-primary': '#f1f1f1',
+      bg: '#0F172A',
+      purple: '#7e5bef',
+    },
     extend: {},
   },
   plugins: [],
