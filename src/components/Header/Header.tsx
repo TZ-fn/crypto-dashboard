080efc1d1c187fdf53a8c58156ab4cacadeb86d3
Tomek <tomekz@vp.pl>
2023-03-14 21:51:03 +0100

    feat: move the style reset to the index.css file, add more styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import MainLogo from './MainLogo/MainLogo';
 
 function Header() {
   return (
-    <header className='p-6 bg-bg border-b-1'>
+    <header className='p-6 bg-bg border-b border-text-primary'>
       <MainLogo />
     </header>
   );
