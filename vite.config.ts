d86b03687f7dc6956a200a2b8502f615509b9379
Tomek <tomekz@vp.pl>
2023-03-15 16:39:01 +0100

    fix: remove an obsolete Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/styles/globals.scss"; @import "src/styles/variables.scss"; @import "src/styles/mixins.scss";`,
-      },
-    },
-  },
 });
