48780b1afb6bb20c0677f9a77b0899735ccc270d
Tomek <tomekz@vp.pl>
2023-03-16 17:54:07 +0100

    feat: add a border color, improve the styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import MainLogo from './MainLogo/MainLogo';
 
 function Header() {
   return (
-    <header className='p-6 bg-bg border-b border-text-primary'>
+    <header className='py-6 px-12 bg-bg border-b border-border'>
       <MainLogo />
     </header>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      'text-primary': '#f1f1f1',
-      bg: '#0F172A',
+      'text-primary': 'hsl(0, 0%, 95%)',
+      bg: 'hsl(222, 47%, 11%)',
+      border: 'hsla(0, 0%, 100%, 0.2)',
     },
     extend: {},
   },
