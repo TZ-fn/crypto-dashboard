710b6377d323d9a811c39be3a804953a6e3bee2e
Tomek <tomekz@vp.pl>
2023-03-17 20:37:58 +0100

    feat: add a logo image

--- src/components/Header/MainLogo/MainLogo.tsx
+++ src/components/Header/MainLogo/MainLogo.tsx
@@ -1,5 +1,12 @@
+import cryptoLogo from 'assets/cryptoLogo.svg';
+
 function MainLogo() {
-  return <h1 className='text-4xl text-text-primary'>Crypto Dashboard</h1>;
+  return (
+    <div className='flex'>
+      <img className='w-20 mr-4' src={cryptoLogo} alt='' />
+      <h1 className='text-4xl text-text-primary self-center'>Crypto Dashboard</h1>
+    </div>
+  );
 }
 
 export default MainLogo;
