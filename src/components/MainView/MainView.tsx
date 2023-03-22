86fcc8319cfd865256d9d45435219d9d57930150
Tomek <tomekz@vp.pl>
2023-03-22 21:15:22 +0100

    fix: improve the styling

--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 function MainView({ children }: { children: ReactNode }) {
-  return <div className=''>{children}</div>;
+  return <div className='px-6 py-12'>{children}</div>;
 }
 
 export default MainView;
