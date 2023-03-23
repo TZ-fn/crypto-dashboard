827a23c29d32ae27a8275a032819ce3d96dbc905
Tomek <tomekz@vp.pl>
2023-03-23 19:53:28 +0100

    feat: improve the styling, add a new color to the theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
     colors: {
       'text-primary': 'hsl(0, 0%, 95%)',
       bg: 'hsl(222, 47%, 11%)',
+      'bg-lighter': 'hsl(222, 30%, 20%)',
       border: 'hsla(0, 0%, 100%, 0.1)',
     },
     extend: {},
