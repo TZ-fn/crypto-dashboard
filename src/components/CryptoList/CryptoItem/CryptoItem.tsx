3a935be91674d52d6bc0bb284b4d3ff7d5a79e72
Tomek <tomekz@vp.pl>
2023-03-23 19:53:57 +0100

    feat: add a CryptoItem component

---
+++ src/components/CryptoList/CryptoItem/CryptoItem.tsx
@@ -0,0 +1,5 @@
+function CryptoItem() {
+  return <li className='my-2 px-3 py-2 bg-bg-lighter rounded-sm'>crypto1</li>;
+}
+
+export default CryptoItem;
--- src/components/CryptoList/CryptoList.tsx
+++ src/components/CryptoList/CryptoList.tsx
@@ -1,5 +1,18 @@
+import CryptoItem from './CryptoItem/CryptoItem';
+
 function CryptoList() {
-  return <ul className='border border-border'>list</ul>;
+  return (
+    <ul className='px-2 border border-border rounded-sm'>
+      <CryptoItem />
+      <CryptoItem />
+      <CryptoItem />
+      <CryptoItem />
+      <CryptoItem />
+      <CryptoItem />
+      <CryptoItem />
+      <CryptoItem />
+    </ul>
+  );
 }
 
 export default CryptoList;
