ae60c30de03eda4166e2a2ff4ce4c2626c850404
Tomek <tomekz@vp.pl>
2023-04-17 21:58:46 +0200

    feat: add a loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+function LoadingSpinner() {
+  return (
+    <div className='relative'>
+      <div className='w-20 h-20 border-bg-lighter border-4 rounded-full'></div>
+      <div className='w-20 h-20 border-text-primary border-t-4 animate-spin rounded-full absolute left-0 top-0'></div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
