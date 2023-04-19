7a05f278b3c60223eb57831336f843d41b6d7f41
Tomek <tomekz@vp.pl>
2023-04-19 19:56:00 +0200

    feat: add more fields and improve the formatting

--- src/components/CoinList/CoinListItem/CoinListItem.tsx
+++ src/components/CoinList/CoinListItem/CoinListItem.tsx
@@ -1,15 +1,17 @@
 interface CoinListItemProps {
+  index: number;
   name: string;
-  price: number;
-  volume24h: number;
+  price: string;
+  volume24h: string;
 }
 
-function CoinListItem({ name, price, volume24h }: CoinListItemProps) {
+function CoinListItem({ index, name, price, volume24h }: CoinListItemProps) {
   return (
-    <li className='flex flex-row align-center justify-around my-4 px-6 py-8 bg-bg-lighter rounded hover:bg-bg-lighter-2 cursor-pointer'>
-      <p>{name}</p>
-      <p>{price}</p>
-      <p>{volume24h}</p>
+    <li className='flex flex-row justify-around  align-center my-4 px-6 py-8 pl-16 bg-bg-lighter rounded hover:bg-bg-lighter-2 cursor-pointer text-lg'>
+      <p className='mr-auto'>{index}</p>
+      <p className='mr-auto'>{name}</p>
+      <p className='mr-auto'>{price}</p>
+      <p className='mr-auto'>{volume24h}</p>
     </li>
   );
 }
