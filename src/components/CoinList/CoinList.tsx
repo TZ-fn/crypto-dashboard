bd5b9e237fc140640bb27c13d44c10d4756e78fe
Tomek <tomekz@vp.pl>
2023-04-19 19:56:25 +0200

    feat: add a table head and improve the styling

--- src/components/CoinList/CoinList.tsx
+++ src/components/CoinList/CoinList.tsx
@@ -1,7 +1,8 @@
 import CoinListItem from './CoinListItem/CoinListItem';
 import Coin from 'types/Coint';
 import { useQuery } from '@tanstack/react-query';
-import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import LoadingSpinner from 'components/LoadingSpinner/LoadingSpinner';
+import formatCurrency from 'utils/formatCurrency';
 
 function CryptoList() {
   const { isLoading, error, data, isFetching } = useQuery({
@@ -15,22 +16,41 @@ function CryptoList() {
 
   if (isLoading) return <LoadingSpinner />;
 
-  if (error) return 'An error has occurred: ' + error.message;
+  if (error) return <p>{`An error has occurred: ${error.message}`}</p>;
 
   return (
-    <ul className='px-2 min-w-full border border-border rounded'>
-      {data.data.map(
-        ({
-          id,
-          name,
-          quote: {
-            USD: { price, volume_24h },
-          },
-        }: Coin) => (
-          <CoinListItem key={id} name={name} price={price} volume24h={volume_24h} />
-        ),
-      )}
-    </ul>
+    <>
+      {isFetching && <p>Updating ...</p>}
+
+      <ul className='px-2 min-w-full border border-border rounded'>
+        <li className='flex flex-row my-4 px-6 py-8 pl-16 bg-bg-lighter rounded hover:bg-bg-lighter-2 cursor-pointer text-lg'>
+          <p className='mr-auto'>#</p>
+          <p className='mr-auto'>Name</p>
+          <p className='mr-auto'>Price</p>
+          <p className='mr-auto'>Volume (24h)</p>
+        </li>
+        {data.data.map(
+          (
+            {
+              id,
+              name,
+              quote: {
+                USD: { price, volume_24h },
+              },
+            }: Coin,
+            index: number,
+          ) => (
+            <CoinListItem
+              key={id}
+              index={index + 1}
+              name={name}
+              price={formatCurrency(price)}
+              volume24h={formatCurrency(volume_24h)}
+            />
+          ),
+        )}
+      </ul>
+    </>
   );
 }
 
