85625893ad6249c792aa6effc863e75906cf52c2
Tomek <tomekz@vp.pl>
2023-05-12 16:41:48 +0200

    feat: add the context data to the CoinDetails view

--- src/components/CoinDetails/CoinDetails.tsx
+++ src/components/CoinDetails/CoinDetails.tsx
@@ -2,9 +2,24 @@ import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import capitalise from 'utils/capitalise';
 import CoinsContext from '~/context/CoinsContext';
+import ContextType from '~/types/ContextType';
 
 function CoinDetails() {
   let params = useParams();
+  const contextData = useContext(CoinsContext) as ContextType;
+  const { isFetching, isLoading, data, error } = contextData.latestData;
+  const {
+    isFetching: metaIsFetching,
+    isLoading: metaIsLoading,
+    metaData,
+    error: metaError,
+  } = contextData.metaData;
+
+  function getCoinLogo(coinID: number): string | undefined {
+    if (metaData) {
+      return metaData.data[coinID].logo;
+    }
+  }
 
   return <div>CoinDetails: {params.name && capitalise(params.name)}</div>;
 }
