50151af59c119cf44d492de145b78dc8ff75ef18
Tomek <tomekz@vp.pl>
2023-06-20 22:01:42 +0200

    fix: improve the styling and responsiveness

--- src/components/CoinList/CoinListItem/CoinListItem.tsx
+++ src/components/CoinList/CoinListItem/CoinListItem.tsx
@@ -18,15 +18,15 @@ function CoinListItem({ index, logo, name, symbol, price, volume24h }: CoinListI
       onClick={() => navigate(`details/${name.toLocaleLowerCase()}`)}
       className='border-b-2 bg-bg-lighter hover:bg-bg-lighter-2 cursor-pointer text-lg'
     >
-      <td className='px-12 py-6 rounded-l'>{index + 1}</td>
-      <td className='px-12 py-6'>
+      <td className='lg:px-12 py-6 rounded-l'>{index + 1}</td>
+      <td className='lg:px-12 py-6'>
         {logo ? <img className='max-w-[4rem]' src={logo} alt='' /> : <LoadingSpinner />}
       </td>
-      <td className='px-12 py-6'>
+      <td className='lg:px-12 py-6'>
         {name} - {symbol}
       </td>
-      <td className='px-12 py-6'>{price}</td>
-      <td className='px-12 py-6 rounded-r'>{volume24h}</td>
+      <td className='lg:px-12 py-6'>{price}</td>
+      <td className='lg:px-12 py-6 rounded-r'>{volume24h}</td>
     </tr>
   );
 }
