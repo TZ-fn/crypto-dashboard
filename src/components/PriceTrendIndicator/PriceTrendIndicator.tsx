fd908bfda8ff176f0364886ed3e2b7a183a70c7f
Tomek <tomekz@vp.pl>
2023-07-01 18:24:30 +0200

    fix: improve the responsiveness

--- src/components/PriceTrendIndicator/PriceTrendIndicator.tsx
+++ src/components/PriceTrendIndicator/PriceTrendIndicator.tsx
@@ -2,7 +2,7 @@ import ArrowUp from 'assets/icons/ArrowUp.png';
 import ArrowDown from 'assets/icons/ArrowDown.png';
 
 function PriceTrendIndicator({ trend }: { trend: 'up' | 'down' }) {
-  return <img className='mx-6 w-[6vw] max-w-[6rem]' src={trend === 'up' ? ArrowUp : ArrowDown} />;
+  return <img className='mx-6 w-[16vw] max-w-[6rem]' src={trend === 'up' ? ArrowUp : ArrowDown} />;
 }
 
 export default PriceTrendIndicator;
