0fa56cccf1733077332b286347c4f2fe2e633d52
Tomek <tomekz@vp.pl>
2023-08-02 19:54:32 +0200

    fix: correct the Header tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Header from './Header';
 
+function renderHeaderWithRouter() {
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>,
+  );
+}
+
 describe('Header', () => {
   it('renders Header', () => {
-    render(<Header />);
+    renderHeaderWithRouter();
     expect(screen.getByRole('heading', { name: /crypto dashboard/i })).toBeInTheDocument();
   });
 });
