f0841d0ae0f4fbbf096d8c7e09e987d8c2a462d4
Tomek <tomekz@vp.pl>
2023-08-03 18:34:25 +0200

    fix: add an alt attribute

--- src/components/PriceTrendIndicator/PriceTrendIndicator.tsx
+++ src/components/PriceTrendIndicator/PriceTrendIndicator.tsx
@@ -2,7 +2,13 @@ import ArrowUp from 'assets/icons/ArrowUp.png';
 import ArrowDown from 'assets/icons/ArrowDown.png';
 
 function PriceTrendIndicator({ trend }: { trend: 'up' | 'down' }) {
-  return <img className='mx-6 w-[16vw] max-w-[6rem]' src={trend === 'up' ? ArrowUp : ArrowDown} />;
+  return (
+    <img
+      className='mx-6 w-[16vw] max-w-[6rem]'
+      src={trend === 'up' ? ArrowUp : ArrowDown}
+      alt={trend === 'up' ? 'Green arrow pointing upwards' : 'Red arrow pointing downwards'}
+    />
+  );
 }
 
 export default PriceTrendIndicator;
