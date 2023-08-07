cbac907aff8c89c96dbb214b360ad76925f8af30
Tomek <tomekz@vp.pl>
2023-08-07 21:44:36 +0200

    fix: CoinDetails tests now work correctly

--- src/components/CoinDetails/CointDetails.test.tsx
+++ src/components/CoinDetails/CointDetails.test.tsx
@@ -1,15 +1,36 @@
 import { render, screen } from '@testing-library/react';
-
+import { Params } from 'react-router-dom';
+import { vi } from 'vitest';
+import CoinsContext from 'context/CoinsContext';
+import mockedContextData from 'tests/mockedContextData';
 import CoinDetails from './CoinDetails';
 
-function renderCoinDetailsWithRouter() {
-  render(<CoinDetails />);
+const coinsName = mockedContextData.latestData.data.data[0].name.toLocaleLowerCase();
+const nameRegex = new RegExp(coinsName, 'i');
+
+function renderCoinDetailsWithContext() {
+  vi.mock('react-router-dom', () => ({
+    useParams: (): Readonly<Params<string>> => ({ name: coinsName }),
+  }));
+
+  render(
+    <CoinsContext.Provider value={mockedContextData}>
+      <CoinDetails />
+    </CoinsContext.Provider>,
+  );
 }
+
+console.log(nameRegex);
 
 describe('CoinDetails', () => {
   it('renders CoinDetails', () => {
-    renderCoinDetailsWithRouter();
+    renderCoinDetailsWithContext();
     expect(screen.getByText(/current price/i)).toBeInTheDocument();
     expect(screen.getByText(/subreddit/i)).toBeInTheDocument();
+  });
+
+  it("renders CoinDetails with current coin's data", () => {
+    renderCoinDetailsWithContext();
+    expect(screen.getByRole('heading', { name: nameRegex })).toBeInTheDocument();
   });
 });
