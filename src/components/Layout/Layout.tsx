1a7fbd6e7c79da4d539e6ce10b678b3943e004d0
Tomek <tomekz@vp.pl>
2023-08-11 16:03:21 +0200

    feat: move the Coin context to a separate file, WIP

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,54 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import MainView from '../MainView/MainView';
-import { useQuery } from '@tanstack/react-query';
-import queryLatestData from 'queryFunctions/queryLatestData';
-import Coin from '~/types/Coin';
-import queryMetaData from 'queryFunctions/queryMetaData';
-import CoinsContext from '~/context/CoinsContext';
+import CoinContextProvider from '~/providers/CoinContextProvider';
 
 export default function Layout() {
-  const { isLoading, isFetching, error, data } = useQuery({
-    queryKey: ['latest'],
-    queryFn: queryLatestData,
-  });
-
-  const coinIDs = data?.data.map((coin: Coin) => coin.id).join(',');
-
-  const {
-    isLoading: metaIsLoading,
-    isFetching: metaIsFetching,
-    error: metaError,
-    data: meta,
-  } = useQuery({
-    queryKey: ['meta', coinIDs],
-    queryFn: () => queryMetaData(coinIDs),
-    enabled: !!coinIDs,
-  });
-
-  const contextData = {
-    latestData: {
-      isLoading: isLoading,
-      isFetching: isFetching,
-      data: data,
-      error: error,
-    },
-    metaData: {
-      isLoading: metaIsLoading,
-      isFetching: metaIsFetching,
-      metaData: meta,
-      error: metaError,
-    },
-  };
-
   return (
-    <CoinsContext.Provider value={contextData}>
-      <div>
-        <Header />
-        <MainView>
-          <Outlet />
-        </MainView>
-      </div>
-    </CoinsContext.Provider>
+    <CoinContextProvider>
+      <Header />
+      <MainView>
+        <Outlet />
+      </MainView>
+    </CoinContextProvider>
   );
 }
---
+++ src/providers/CoinContextProvider.tsx
@@ -0,0 +1,51 @@
+import { ReactNode, useContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import queryLatestData from '~/queryFunctions/queryLatestData';
+import queryMetaData from '~/queryFunctions/queryMetaData';
+import CoinsContext from 'context/CoinsContext';
+import Coin from '~/types/Coin';
+
+interface CoinContextProviderProps {
+  children: ReactNode;
+}
+
+export default function CoinContextProvider({ children }: CoinContextProviderProps) {
+  const { isLoading, isFetching, error, data } = useQuery({
+    queryKey: ['latest'],
+    queryFn: queryLatestData,
+  });
+
+  const coinIDs = data?.data.map((coin: Coin) => coin.id).join(',');
+
+  const {
+    isLoading: metaIsLoading,
+    isFetching: metaIsFetching,
+    error: metaError,
+    data: meta,
+  } = useQuery({
+    queryKey: ['meta', coinIDs],
+    queryFn: () => queryMetaData(coinIDs),
+    enabled: !!coinIDs,
+  });
+
+  const contextData = {
+    latestData: {
+      isLoading: isLoading,
+      isFetching: isFetching,
+      data: data,
+      error: error,
+    },
+    metaData: {
+      isLoading: metaIsLoading,
+      isFetching: metaIsFetching,
+      metaData: meta,
+      error: metaError,
+    },
+  };
+
+  const coinContext = contextData;
+  const metaCoinData = coinContext?.metaData;
+  const latestCoinData = coinContext?.latestData;
+
+  return <CoinsContext.Provider value={coinContext}>{children}</CoinsContext.Provider>;
+}
