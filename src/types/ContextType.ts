f5e2e3916fdf3dd03bf8aff48d1e7d01ca315331
Tomek <tomekz@vp.pl>
2023-08-17 19:38:57 +0200

    fix: add proper types for Coins' and CoinsMeta' data

--- src/types/ContextType.ts
+++ src/types/ContextType.ts
@@ -1,14 +1,17 @@
+import Coin from './Coin';
+import CoinMeta from './CoinMeta';
+
 export default interface ContextType {
   latestData: {
     isLoading: boolean;
     isFetching: boolean;
-    data: any;
+    data: { data: Coin[] };
     error: unknown;
   };
   metaData: {
     isLoading: boolean;
     isFetching: boolean;
-    metaData: any;
+    metaData: { data: CoinMeta[] };
     error: unknown;
   };
 }
