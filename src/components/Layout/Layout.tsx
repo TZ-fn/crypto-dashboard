288e2caa968de402c8f96fee8b7610888c2322b1
Tomek <tomekz@vp.pl>
2023-08-21 20:26:27 +0200

    chore: remove an obsolete import

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import MainView from '../MainView/MainView';
-import CoinContextProvider from '~/providers/CoinContextProvider';
 
 export default function Layout() {
   return (
