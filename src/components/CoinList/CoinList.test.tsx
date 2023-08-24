c1aa17937bf00c09ce1504b995b9858fa369cd51
Tomek <tomekz@vp.pl>
2023-08-24 21:05:01 +0200

    feat: add more tests for the CoinList component

--- src/components/CoinList/CoinList.test.tsx
+++ src/components/CoinList/CoinList.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as reactQuery from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import CoinContextProvider from 'providers/CoinContextProvider';
 import CoinList from './CoinList';
+
+vi.spyOn(reactQuery, 'useQuery').mockImplementation(
+  jest.fn().mockReturnValue({ data: { ...MockData }, isLoading: false, isSuccess: true }),
+);
 
 function renderCoinListWithContext() {
   const queryClient = new QueryClient();
