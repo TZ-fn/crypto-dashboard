6bbfefdf53742cbc476e8b362195128226dcfa08
Tomek <tomekz@vp.pl>
2023-08-25 17:27:42 +0200

    feat: add an error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import CoinContextProvider from './providers/CoinContextProvider';
 import CoinList from './components/CoinList/CoinList';
 import CoinDetails from './components/CoinDetails/CoinDetails';
@@ -11,16 +12,18 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <div>
-      <QueryClientProvider client={queryClient}>
-        <CoinContextProvider>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route index element={<CoinList />} />
-              <Route path='/details/:name' element={<CoinDetails />} />
-            </Route>
-          </Routes>
-        </CoinContextProvider>
-      </QueryClientProvider>
+      <ErrorBoundary>
+        <QueryClientProvider client={queryClient}>
+          <CoinContextProvider>
+            <Routes>
+              <Route path='/' element={<Layout />}>
+                <Route index element={<CoinList />} />
+                <Route path='/details/:name' element={<CoinDetails />} />
+              </Route>
+            </Routes>
+          </CoinContextProvider>
+        </QueryClientProvider>
+      </ErrorBoundary>
     </div>
   );
 }
