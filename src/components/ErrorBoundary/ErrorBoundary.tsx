0ce43b1de9b8b42bec7db3775917606357cd3df6
Tomek <tomekz@vp.pl>
2023-08-26 20:06:03 +0200

    feat: add logic, typing and styling

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,29 +1,37 @@
-import { Component } from 'react';
+import { Component, ReactNode } from 'react';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import styles from './ErrorBoundary.module.scss';
 
-class ErrorBoundary extends Component {
-  constructor(props) {
+type ErrorBoundaryProps = {
+  children: ReactNode;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
+  static getDerivedStateFromError() {
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
-    // Example "componentStack":
-    //   in ComponentThatThrows (created by App)
-    //   in ErrorBoundary (created by App)
-    //   in div (created by App)
-    //   in App
-    logErrorToMyService(error, info.componentStack);
+  componentDidCatch(error: Error) {
+    console.log(error);
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return this.props.fallback;
+      return (
+        <div className='flex flex-col mt-32 items-center'>
+          <p className='text-xl'>Something went wrong, please try again later.</p>
+          <LoadingSpinner />
+        </div>
+      );
     }
 
     return this.props.children;
