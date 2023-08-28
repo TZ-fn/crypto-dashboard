36ed3c0fdd4c009a53c27712e78d3b4a9debc0b5
Tomek <tomekz@vp.pl>
2023-08-28 17:08:46 +0200

    feat: fix the React Query mock

--- src/components/CoinList/CoinList.test.tsx
+++ src/components/CoinList/CoinList.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import * as reactQuery from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import CoinContextProvider from 'providers/CoinContextProvider';
 import CoinList from './CoinList';
+import mockedContextData from '~/tests/mockedContextData';
 
-vi.spyOn(reactQuery, 'useQuery').mockImplementation(
-  jest.fn().mockReturnValue({ data: { ...MockData }, isLoading: false, isSuccess: true }),
-);
+vi.mock('@tanstack/react-query', async () => {
+  const actual = await vi.importActual('@tanstack/react-query');
+  return {
+    ...actual,
+    useQuery: vi.fn().mockReturnValue({ data: { data: [] }, isLoading: false, error: {} }),
+  };
+});
 
 function renderCoinListWithContext() {
   const queryClient = new QueryClient();
