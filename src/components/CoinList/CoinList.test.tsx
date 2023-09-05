c0574085d02d0f28ea7129d7665cf48de74074fe
Tomek <tomekz@vp.pl>
2023-09-05 20:14:11 +0200

    fix: mock the API data properly by using context provider with mocked data

--- src/components/CoinList/CoinList.test.tsx
+++ src/components/CoinList/CoinList.test.tsx
@@ -1,33 +1,15 @@
 import { render, screen } from '@testing-library/react';
-import { vi } from 'vitest';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import CoinContextProvider from 'providers/CoinContextProvider';
 import CoinList from './CoinList';
-import mockedContextData from 'tests/mockedContextData';
-
-vi.doMock('@tanstack/react-query', async () => {
-  const actual = await vi.importActual('@tanstack/react-query');
-  if (typeof actual === 'object') {
-    return {
-      ...actual,
-      useQuery: vi
-        .fn()
-        .mockReturnValue({ data: { data: mockedContextData }, isLoading: false, error: {} }),
-    };
-  }
-});
+import mockedContextData from '~/tests/mockedContextData';
+import CoinsContext from 'context/CoinsContext';
 
 function renderCoinListWithContext() {
-  const queryClient = new QueryClient();
-
   render(
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <CoinContextProvider>
-          <CoinList />
-        </CoinContextProvider>
-      </QueryClientProvider>
+      <CoinsContext.Provider value={mockedContextData}>
+        <CoinList />
+      </CoinsContext.Provider>
     </BrowserRouter>,
   );
 }
