7e4d7ba6f68542bc2836ebb9438ed11f53183206
Tomek <tomekz@vp.pl>
2023-09-10 18:47:33 +0200

    fix: add a test ID to the component for easier testing

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 function LoadingSpinner() {
   return (
-    <div className='relative mt-8'>
+    <div data-testid='loading-spinner' className='relative mt-8'>
       <div className='h-20 w-20 rounded-full border-4 border-bg-lighter'></div>
       <div className='absolute left-0 top-0 h-20 w-20 animate-spin rounded-full border-t-4 border-text-primary'></div>
     </div>
