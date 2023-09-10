537dc1b8a73759435a1529e6693e035df36588a0
Tomek <tomekz@vp.pl>
2023-09-10 18:57:47 +0200

    feat: add more tests to cover all table cells

--- src/components/CoinList/CoinListItem/CoinListItem.test.tsx
+++ src/components/CoinList/CoinListItem/CoinListItem.test.tsx
@@ -3,11 +3,13 @@ import { MemoryRouter } from 'react-router-dom';
 import CoinListItem from './CoinListItem';
 import formatCurrency from 'utils/formatCurrency';
 
+let coinIndex = 1;
+
 function renderCoinListItem() {
   render(
     <MemoryRouter>
       <CoinListItem
-        index={1}
+        index={coinIndex}
         logo={''}
         name={'Bitcoin'}
         symbol={'BTC'}
@@ -22,6 +24,8 @@ describe('CoinListItem', () => {
   it('renders correctly', async () => {
     renderCoinListItem();
 
+    expect(screen.getByRole('cell', { name: `${coinIndex + 1}` })).toBeInTheDocument();
+    expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /bitcoin \- btc/i })).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /\$100.00/i })).toBeInTheDocument();
   });
