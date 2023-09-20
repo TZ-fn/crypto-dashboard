b89ee177607f381eb1abbe1eadaba3b4f818ccbf
Tomek <tomekz@vp.pl>
2023-09-20 20:51:07 +0200

    feat: add more styling to the Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import FavouritesList from '../FavouritesList/FavouritesList';
 
 function Sidebar() {
   return (
-    <aside className='flex flex-col items-center justify-center gap-4 rounded bg-bg-lighter px-4 py-6 '>
+    <aside className='flex flex-col items-center justify-center gap-4 rounded bg-bg-lighter px-4 py-6 min-[1850px]:fixed min-[1850px]:left-[3vw] min-[1850px]:top-64 '>
       <FavouritesList />
     </aside>
   );
