37fb6fab8dbb9496a433c800c964456a0b5abf74
Tomek <tomekz@vp.pl>
2023-09-22 19:25:31 +0200

    feat: refactor component's logic

--- src/components/FavouritesList/FavouritesList.tsx
+++ src/components/FavouritesList/FavouritesList.tsx
@@ -1,24 +1,25 @@
 import useFavourites from 'hooks/useFavourites';
+import FavouritesListElement from './FavouritesListElement/FavouritesListElement';
 
 function FavouritesList() {
   const [favourites, setFavourites] = useFavourites();
 
   return (
-    <div className='flex'>
-      {favourites.map((favourite, index) => (
-        <p key={index}>{favourite.name}</p>
+    <ul className='flex'>
+      {favourites.map(({ id, name, logo }) => (
+        <FavouritesListElement key={id} name={name} logo={logo} />
       ))}
       <button
         className='mx-2'
         onClick={() => {
           if (typeof setFavourites === 'function') {
-            setFavourites([{ id: 1, name: 'asd', logo: 'sdf' }]);
+            setFavourites((prev) => [...prev, { id: 1, name: 'BT', logo: 'logo' }]);
           }
         }}
       >
-        asdaf
+        Click
       </button>
-    </div>
+    </ul>
   );
 }
 
