3763d9d2778416bb6039c8190829a5433a6fb33a
Tomek <tomekz@vp.pl>
2023-09-25 21:19:55 +0200

    feat: add a button for clearing the favourites, improve the styling

--- src/components/FavouritesList/FavouritesList.tsx
+++ src/components/FavouritesList/FavouritesList.tsx
@@ -5,7 +5,7 @@ function FavouritesList() {
   const [favourites, setFavourites] = useFavourites();
 
   return (
-    <ul className='flex'>
+    <ul className='flex flex-col'>
       {favourites.map(({ id, name, logo }) => (
         <FavouritesListElement key={id} name={name} logo={logo} />
       ))}
@@ -13,11 +13,25 @@ function FavouritesList() {
         className='mx-2'
         onClick={() => {
           if (typeof setFavourites === 'function') {
-            setFavourites((prev) => [...prev, { id: 1, name: 'BT', logo: 'logo' }]);
+            setFavourites((prev) => [
+              ...prev,
+              {
+                id: 1,
+                name: 'BitCoin',
+                logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+              },
+            ]);
           }
         }}
       >
         Click
+      </button>
+      <button
+        onClick={() => {
+          setFavourites(() => []);
+        }}
+      >
+        Clear
       </button>
     </ul>
   );
--- src/components/FavouritesList/FavouritesListElement/FavouritesListElement.tsx
+++ src/components/FavouritesList/FavouritesListElement/FavouritesListElement.tsx
@@ -2,8 +2,12 @@ import LoadingSpinner from 'components/LoadingSpinner/LoadingSpinner';
 
 function FavouritesListElement({ name, logo }: Omit<FavouriteCoin, 'id'>) {
   return (
-    <li>
-      {logo ? <img className='min-w-[2rem] max-w-[2vw]' src={logo} alt='' /> : <LoadingSpinner />}
+    <li className='m-2 flex items-center'>
+      {logo ? (
+        <img className='mx-2 min-w-[2rem] max-w-[1.5vw]' src={logo} alt='' />
+      ) : (
+        <LoadingSpinner />
+      )}
       <p>{name}</p>
     </li>
   );
