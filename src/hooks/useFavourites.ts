873f756b6600eb3454c0411d53cd5a0db8e581f0
Tomek <tomekz@vp.pl>
2023-10-04 21:09:23 +0200

    refactor: refactor how favourites are handled

--- src/hooks/useFavourites.ts
+++ src/hooks/useFavourites.ts
@@ -3,7 +3,16 @@ import FavouriteCoin from 'types/FavouriteCoin';
 
 const useFavourites = () => {
   const [favourites, setFavourites] = useLocalStorage<FavouriteCoin[]>('favourites', []);
-  return [favourites, setFavourites] as const;
+
+  function handleFavourites(id: number, logo: string | undefined, name: string) {
+    if (favourites.filter((coin) => coin.name === name)) {
+      setFavourites((prev) => prev.filter((coin) => coin.name === name));
+    } else {
+      setFavourites((prev) => [...prev, { id, name, logo }]);
+    }
+  }
+
+  return [favourites, handleFavourites] as const;
 };
 
 export default useFavourites;
