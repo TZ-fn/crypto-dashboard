9e924d6e29da7d9493ea7bab79ea6dc27e26acf3
Tomek <tomekz@vp.pl>
2023-10-19 10:59:44 +0200

    feat: add sorting to the list

--- src/components/FavouritesList/FavouritesList.tsx
+++ src/components/FavouritesList/FavouritesList.tsx
@@ -4,13 +4,15 @@ import FavouritesListElement from './FavouritesListElement/FavouritesListElement
 function FavouritesList() {
   const [favourites, setFavourites] = useFavourites();
 
+  const sortedFavourites = favourites.sort((a, b) => a.id - b.id);
+
   return (
     favourites.length > 0 && (
       <ul className='flex flex-col'>
         <h2 className='border-b-[1px] border-solid border-border bg-bg-lighter-2 p-4 text-center'>
           Favourites
         </h2>
-        {favourites.map(({ id, name, logo }) => (
+        {sortedFavourites.map(({ id, name, logo }) => (
           <FavouritesListElement
             id={id}
             key={name}
