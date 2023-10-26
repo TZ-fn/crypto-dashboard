9ba205bdbdaa7fed2859699f18582899d573999f
Tomek <tomekz@vp.pl>
2023-10-26 16:57:29 +0200

    feat: add a button for controlling visibility of the list

--- src/components/FavouritesList/FavouritesList.tsx
+++ src/components/FavouritesList/FavouritesList.tsx
@@ -1,20 +1,31 @@
 import useFavourites from 'hooks/useFavourites';
 import FavouritesListElement from './FavouritesListElement/FavouritesListElement';
+import { useState } from 'react';
+import SortTypeIndicator from '../SortTypeIndicator/SortTypeIndicator';
 
 function FavouritesList() {
   const [favourites, setFavourites] = useFavourites();
+  const [isListVisible, setIsListVisible] = useState(true);
 
   const sortedFavourites = favourites.sort((coin1, coin2) => new Intl.Collator('en').compare(coin1.name, coin2.name));
 
   return favourites.length > 0 ? (
-    <ul className='flex flex-col'>
-      <h2 className=' my-1.5 select-none rounded bg-bg-lighter-2 p-2 text-center font-bold sm:p-2 md:p-5 lg:p-6'>
-        Favourites
-      </h2>
-      {sortedFavourites.map(({ id, name, logo }) => (
-        <FavouritesListElement id={id} key={name} name={name} logo={logo} deletingFunction={setFavourites} />
-      ))}
-    </ul>
+    <div className='flex flex-col'>
+      <div className='flex min-w-[14rem] select-none justify-between rounded bg-bg-lighter-2 p-2 text-center font-bold sm:p-2 md:p-5 lg:p-6'>
+        <h2>Favourites</h2>
+        <button onClick={() => setIsListVisible((prev) => !prev)}>
+          <SortTypeIndicator type={isListVisible ? 'ascending' : 'descending'} />
+          {isListVisible}
+        </button>
+      </div>
+
+      <ul>
+        {isListVisible &&
+          sortedFavourites.map(({ id, name, logo }) => (
+            <FavouritesListElement id={id} key={name} name={name} logo={logo} deletingFunction={setFavourites} />
+          ))}
+      </ul>
+    </div>
   ) : null;
 }
 
