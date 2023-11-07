4bc91f8c7e84e81d230745f9f47388c57dec8fc4
Tomek <tomekz@vp.pl>
2023-11-07 18:22:57 +0100

    feat: mock the useFavourites hook WIP

--- src/components/FavouritesList/FavouritesList.test.tsx
+++ src/components/FavouritesList/FavouritesList.test.tsx
@@ -1,9 +1,18 @@
+import { vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import FavouritesList from './FavouritesList';
 import mockedContextData from 'tests/mockedContextData';
 import CoinsContext from 'context/CoinsContext';
+
+vi.mock('hooks/useFavourites', () => ({
+  useFavourites: () => [
+    { id: 0, logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png', name: 'Bitcoin' },
+    { id: 3, logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png', name: 'BNB' },
+    { id: 1, logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png', name: 'Ethereum' },
+  ],
+}));
 
 function renderFavouritesListWithContext() {
   render(
@@ -20,6 +29,7 @@ describe('FavouritesList', () => {
 
   it('renders CoinList correctly', async () => {
     renderFavouritesListWithContext();
+    screen.debug();
     expect(screen.getByRole('heading', { name: /favourites/i })).toBeInTheDocument();
   });
 });
